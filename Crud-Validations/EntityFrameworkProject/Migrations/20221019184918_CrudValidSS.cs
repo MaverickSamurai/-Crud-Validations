c92ad9e14d60f0e6a102d7de359e98c6409428bf
MaverickSamurai <mirsamirba@code.edu.az>
2022-10-19 22:51:18 +0400

    Crud-Validations

---
+++ Crud-Validations/EntityFrameworkProject/Migrations/20221019184918_CrudValidSS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkProject.Migrations
+{
+    public partial class CrudValidSS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Socials",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 22, 49, 18, 694, DateTimeKind.Local).AddTicks(4007));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 22, 49, 18, 695, DateTimeKind.Local).AddTicks(13));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 22, 49, 18, 695, DateTimeKind.Local).AddTicks(35));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Socials",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 16, 40, 55, 342, DateTimeKind.Local).AddTicks(1114));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 16, 40, 55, 343, DateTimeKind.Local).AddTicks(2649));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 19, 16, 40, 55, 343, DateTimeKind.Local).AddTicks(2690));
+        }
+    }
+}
---
+++ Crud-Validations/EntityFrameworkProject/Models/Social.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityFrameworkProject.Models
+{
+    public class Social : BaseEntity
+    {
+        [Required(ErrorMessage = "Name can't be empty!")]
+        [StringLength(20, ErrorMessage = "The size of text max 20!")]
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
